8bff5be56f8f6b1c3414856a392e181a85728f82
PhucNM <binguyenfpt1407@gmail.com>
2025-05-14 18:09:09 +0700

    Updated

--- Medium_Level_Leetcode/Remove_Duplicates_From_Sorted_Array_II.cpp
+++ Medium_Level_Leetcode/Remove_Duplicates_From_Sorted_Array_II.cpp
@@ -1,1 +1,40 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+    if (nums.size() <= 2)
+    {
+        for (auto const k : nums)
+        {
+            cout << k << " ";
+        }
+        return 0;
+    }
+
+    int j = 2;
+    for (int i = 2; i < nums.size(); i++)
+    {
+        if (nums[i] != nums[i - 2])
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    for (auto const k : nums)
+    {
+        cout << k << " ";
+    }
+}
