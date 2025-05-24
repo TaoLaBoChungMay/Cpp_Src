1277855a16cf3dadbf896e12a7241dfcb71c4c85
PhucNM <binguyenfpt1407@gmail.com>
2025-05-24 07:53:38 +0700

    Changed

--- Medium_Level_Leetcode/Zigzag_Conversion.cpp
+++ Medium_Level_Leetcode/Zigzag_Conversion.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main()
 {
-    int numRows;
     string s;
+    int numRows;
     cin >> s >> numRows;
-    int step = 1, current = 0; // Current cho biết vị trí hiện tại ở dòng thứ mấy của numRows
-    if (numRows <= 1)          // Khi numRows =1 thì sẽ bị tràn bộ nhớ
-    {
-        cout << s;
-        return 0;
-    }
+    int current = 0, step = 1;
     vector<string> Zigzag(numRows);
-    for (char c : s)
+    for (auto const c : s)
     {
         Zigzag[current].push_back(c);
         if (current == 0)
@@ -27,6 +23,6 @@ int main()
     for (auto const c : Zigzag)
     {
         result += c;
-        cout << result;
     }
+    cout << result;
 }
